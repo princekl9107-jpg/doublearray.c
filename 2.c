8e8e7c93a5fd2d4f57af92092e584a983055facb
prince khan <alexanderkl9107@gmail.com>
2026-07-14 20:46:45 +0000

    solve sum of double array

--- 2.c
+++ 2.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 int main()
 {
-    int arr[100][100],i,j;
-    
+    int array[100][100],i,j,sum=0;
+    for(int i=0;i<2;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            printf("enter the number[%d][%d]=",i,j);
+            scanf("%d",&array[i][j]);
+        }
+        printf("\n");
+    }
+
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            sum=sum+array[i][j];
+        }
+      
+    }
+    printf("the sum of double array =%d",sum);
+    printf("\n");
+    return 0;
+     
 }
